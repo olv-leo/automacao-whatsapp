afe7eb37de9cf37c544668238ceeba69a65cf6f0
olv-leo <1996olv.leo@gmail.com>
2022-04-03 18:39:53 -0300

    Começando a criar testes

---
+++ automacao_wpp/tests/test_gerar_links/test_gerar_link_wpp.py
@@ -0,0 +1,4 @@
+from automacao_wpp.gerar_link.gerar_link import gerar_link_wpp
+
+def test_gerar_link_wpp():
+    assert gerar_link_wpp(16997952356) == 'https://api.whatsapp.com/send?phone=5516997952356'
