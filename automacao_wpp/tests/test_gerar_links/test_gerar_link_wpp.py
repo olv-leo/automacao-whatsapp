4828ae7958e672807cb59edce00e2e1aee039dbc
olv-leo <1996olv.leo@gmail.com>
2022-04-03 19:00:06 -0300

    Revert "Começando a criar testes"
    This reverts commit e9a9a8d26bcc3288bde3b3dde08d3137020513e2.

--- automacao_wpp/tests/test_gerar_links/test_gerar_link_wpp.py
+++
@@ -1,7 +0,0 @@
-from automacao_wpp.gerar_link.gerar_link import gerar_link_wpp
-
-
-def test_gerar_link_wpp():
-    assert gerar_link_wpp() == ''
-    assert gerar_link_wpp('16997952356') == 'https://api.whatsapp.com/send?phone=5516997952356'
-    assert gerar_link_wpp('16997952356', 'Teste') == 'https://api.whatsapp.com/send?phone=5516997952356&text=Teste'
