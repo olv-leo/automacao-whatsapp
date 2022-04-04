95aeef619f91089022f7d5ebfdc5c8ce3fc730d5
olv-leo <1996olv.leo@gmail.com>
2022-04-04 12:48:06 -0300

    Parametrizando os testes do Pytest e melhorando a cobertura de testes.

---
+++ automacao_wpp/tests/test_gerar_links/test_excecoes.py
@@ -0,0 +1,14 @@
+from automacao_wpp.gerar_link.gerar_link import gerar_link_wpp, NumeroInvalido
+import pytest
+
+
+@pytest.mark.parametrize(
+    'numero, msg',
+    [
+        ('', ''),
+        ('', 'Teste')
+     ]
+)
+def test_gerar_link_wpp(numero, msg):
+    with pytest.raises(NumeroInvalido):
+        gerar_link_wpp(numero, msg)
--- automacao_wpp/tests/test_gerar_links/test_gerar_link_wpp.py
+++ automacao_wpp/tests/test_gerar_links/test_gerar_link_wpp.py
@@ -1,5 +1,13 @@
 from automacao_wpp.gerar_link.gerar_link import gerar_link_wpp
+import pytest
 
 
-def test_gerar_link_wpp():
-    assert gerar_link_wpp(16997952356) == 'https://api.whatsapp.com/send?phone=5516997952356'
+@pytest.mark.parametrize(
+    'numero, msg, resultado',
+    [
+        ('16997952356', '', 'https://api.whatsapp.com/send?phone=5516997952356'),
+        ('16997952356', 'Teste', 'https://api.whatsapp.com/send?phone=5516997952356&text=Teste'),
+     ]
+)
+def test_gerar_link_wpp(numero, msg, resultado):
+    assert gerar_link_wpp(numero, msg) == resultado
